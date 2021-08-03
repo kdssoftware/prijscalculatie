3733b8e5abe06e5a5cac99274ad91065cec746f9
snakehead007 <snakehead007@protonmail.com>
2021-08-03 12:57:47 +0200

    add table

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,11 @@
-<?php // Silence is golden
+<?php
+global $wpdb;
+if(array_key_exists("prijzentabel_item_add_submit",$_POST)){
+    $wpdb->insert("wp_items",array(
+        "naam"=>$_POST["naam"],
+        "winkelprijs_pp" =>$_POST["winkelprijs_pp"],
+        "winstmarge"=>$_POST["winstmarge"]
+    ));
+}
+
+header("Location: ".$_SERVER['HTTP_REFERER']);
--- prijscalculatie.php
+++ prijscalculatie.php
@@ -5,18 +5,18 @@
  *
  * @link              https://github.com/snakehead007/prijscalculatie
  * @since             1.0.0
- * @package           prijscalculatie
+ * @package           TPX Prijscalculatie 
  *
  * @wordpress-plugin
- * Plugin Name:       Prijscalulatie Transplantouz
- * Plugin URI:        https://github.com/snakehead007/prijscalculatie
+ * Plugin Name:       TPX Prijscalculatie 
+ * Plugin URI:        https://wordpress.org/plugins/TPX-prijscalculatie
  * Description:       Provide prizing and add a page to create qoutes.
  * Version:           1.0.0
  * Author:            Karel De Smet
  * Author URI:        http://karel.be
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       prijscalculatie
+ * Text Domain:       TPX Prijscalculatie 
  * Domain Path:       /languages
  */
 
@@ -81,4 +81,93 @@ function prijscalculatie_formulier(){
 	return $content;
 }
 
+function prijzentabel(){
+	add_menu_page("Prijzentabel aanpassen","TPX Prijzentabel","manage_options","prijzentabel","prijzentabel_page","",200);
+}
+
+
+function prijzentabel_page(){
+	global $wpdb;
+	$items = $wpdb->get_results("SELECT * FROM wp_items",ARRAY_A);
+	?>
+		<table style="width:100%;text-align:center">
+			<thead style="background-color:#000000;color:#ffffff;font-weight:500">
+			<tr>
+				<th>Code</th>
+				<th>Item-naam</th>
+				<th>Winkelprijs/pp</th>
+				<th>Winstmarge</th>
+				<th>Dienstprijs</th>
+				<th>Sponsorbijdrage</th>
+				<th>Edit </th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<form action="<?=get_admin_url()."admin-post.php"?>" method="POST">
+					<td>
+						/
+					</td>
+					<td>
+						<input style="width=100%" type="text" name="naam" id="naam" placeholder="naam van dit item" required/>
+					</td>
+					<td>
+						<input style="width=100%" type="number" oninput="new_item()" name="winkelprijs_pp" id="winkelprijs_pp" placeholder="winkelprijs per persoon" required/>
+					</td>
+					<td>
+						<input style="width=100%" type="number" oninput="new_item()" name="winstmarge" id="winstmarge" placeholder="winstmarge in %" required/>
+					</td>
+					<td id="dienstprijs"></td>
+					<td id="sponsorbijdrage"></td>
+					<td>
+						<input type="submit" name="prijzentabel_item_add_submit" value="Add"/>
+					</td>
+				</form>
+			</tr>
+			<script>
+				function new_item(){
+					let winkelprijs_pp = document.getElementById("winkelprijs_pp").value;
+					let winstmarge = document.getElementById("winstmarge").value;
+					let dienstprijs = document.getElementById("dienstprijs");
+					let sponsorbijdrage = document.getElementById("sponsorbijdrage");
+						console.log(winkelprijs_pp,winstmarge);
+					if(winkelprijs_pp!="" && winstmarge!=""){
+						dienstprijs.innerText= Math.round(Number(winkelprijs_pp)+(Number(winkelprijs_pp)*Number(winstmarge)/100.0))+" EUR";
+						sponsorbijdrage.innerText=Math.round(Number(winkelprijs_pp)+(Number(winkelprijs_pp)*Number(winstmarge)/100.0)) - Number(winkelprijs_pp)+" EUR" ;
+					}else{
+						dienstprijs.innerText="";
+						sponsorbijdrage.innerText="";
+					}
+				}
+			</script>
+			<?php
+			foreach($items as $item){
+				$winkelprijs_pp = intval($item["winkelprijs_pp"]);
+				$winstmarge = intval($item["winstmarge"]);
+				$dienstprijs = round($winkelprijs_pp+($winkelprijs_pp*$winstmarge/100), 0);
+				$sponsorbijdrage = $dienstprijs-$winkelprijs_pp;
+				$ID = intval($item["ID"]);
+				?>
+				<tr style="<?= (($ID % 2 == 1)?"background-color:#CCCCCC":"") ?>">
+					<td><?= $item["ID"]?></td>
+					<td><?= $item["naam"] ?></td>
+					<td><?= $item["winkelprijs_pp"] ?> EUR</td>
+					<td><?= $item["winstmarge"] ?>%</td>
+					<td><?= $dienstprijs ?> EUR</td>
+					<td><?= $sponsorbijdrage ?> EUR</td>
+					<td>
+						<button>Delete</button>
+						<button>Edit</button>
+					</td>
+				</tr>
+			<?php } ?>
+			</tbody>
+		</table>
+	<?php
+}
+
+
 add_shortcode('prijscalculatie_formulier','prijscalculatie_formulier');
+add_action("admin_menu","prijzentabel");
+// add_action("wp_head","prijzentabel_item_add");
+?>
